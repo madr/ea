ea427b4dbcd1c92fc84572c3f3b709a1c07e5755
Anders Ytterström <yttan@fastmail.se>
2022-10-19 12:08:39 +0200

    Add content

--- src/guests/views.py
+++ src/guests/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+@requires_POST
+def rvsp(request):
+    # store to database
+
+    # send confirmation email, if email was provided
+
+    # send confirmation SMS, if phone was provided
+
+    render(request, "ok")
