61ec20ea7f0155e95b79f0a71facb78d3269180c
Anders Ytterström <yttan@fastmail.se>
2022-12-23 15:42:11 +0100

    Rvsp form (#6)
    * Remove guest app
    
    * Add invitation Responses to the Django admin site
    
    * Enable the invitation app
    
    * Create migrations for invitation responses
    
    * Make optional fields not required
    
    * Migrate non required fields
    
    * Add action to mark invitations as verified
    
    * Reformat code
    
    Also add some list filters.
    
    * Add RVSP form to view
    
    * Update default values on migration
    
    * Finalize form

--- src/invitation/admin.py
+++ src/invitation/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
+from .models import InvitationResponse
 
-# Register your models here.
+
+@admin.action(description="Mark selected as verified (mail confirmation has been sent)")
+def make_verified(modeladmin, request, queryset):
+    queryset.update(verified=True)
+
+
+class InvitationResponseAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "count",
+        "ceremony",
+        "party",
+        "accommodation",
+        "acknowledged",
+    )
+    list_filter = (
+        "is_coming",
+        "is_partying",
+        "verified",
+    )
+    ordering = ("-id", "-people_count")
+    actions = [make_verified]
+    readonly_fields = (
+        "is_coming",
+        "is_partying",
+        "phone",
+        "email",
+        "people_count",
+        "children_count",
+        "diet",
+        "note",
+        "consent",
+        "verified",
+    )
+
+    @admin.display(description=_("Antal"))
+    def count(self, obj):
+        pc = obj.people_count if obj.people_count else 0
+        cc = obj.children_count if obj.children_count else 0
+        return "%s (%s)" % (pc + cc, cc)
+
+    @admin.display(description=_("Vigseln?"))
+    def ceremony(self, obj):
+        return "Ja!" if obj.is_coming == "yes" else "Nej"
+
+    @admin.display(description=_("Festen?"))
+    def party(self, obj):
+        return "Ja!" if obj.is_partying == "yes" else "Nej"
+
+    @admin.display(description=_("Campar?"))
+    def accommodation(self, obj):
+        return "-" if obj.camping == "no" else obj.camping
+
+    @admin.display(description=_("Kvittens skickad?"))
+    def acknowledged(self, obj):
+        return "Nej" if obj.verified == False else "Ja"
+
+
+admin.site.register(InvitationResponse, InvitationResponseAdmin)
--- src/invitation/models.py
+++ src/invitation/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.forms import ModelForm, RadioSelect
+from django.utils.translation import gettext as _
 
-# Create your models here.
+COMING_CHOICES = {
+    "yes": "Ja!",
+    "no": "Nej, jag/vi kan tyvärr inte komma",
+}
+
+PARTY_CHOICES = {
+    "yes": "Ja!",
+    "no": "Nej, jag/vi deltar endast vid ceremonin",
+}
+
+CAMPING_CHOICES = {
+    "tent": "Ja, i tält",
+    "rv": "Ja, i Husvagn",
+    "no": "Nej, ingen campingplats",
+}
+
+
+class InvitationResponse(models.Model):
+    name = models.CharField(_("Namn"), max_length=256)
+    email = models.EmailField(_("E-postadress"), help_text="För bekräftelse/kvittens")
+    phone = models.IntegerField(
+        _("Telefonnummer"), help_text="För bekräftelse/kvittens"
+    )
+    is_coming = models.CharField(
+        _("Kommer ni på bröllopet?"),
+        max_length=4,
+        choices=COMING_CHOICES.items(),
+        default="yes",
+    )
+    is_partying = models.CharField(
+        _("Stannar ni till middagen och festen?"),
+        max_length=4,
+        choices=PARTY_CHOICES.items(),
+        default="yes",
+    )
+    people_count = models.SmallIntegerField(_("Antal personer"), blank=True, null=True)
+    children_count = models.SmallIntegerField(_("Antal barn"), blank=True, null=True)
+    diet = models.TextField(_("Specialkost"), blank=True, null=True)
+    camping = models.CharField(
+        _("Campar ni vid Österå Bystuga?"),
+        max_length=4,
+        choices=CAMPING_CHOICES.items(),
+        default="no",
+    )
+    note = models.TextField(_("Övrig kommentar"), blank=True, null=True)
+    consent = models.BooleanField(
+        _(
+            "Ja, brudparet får spara mina uppgifter fram tills juli 2023 för att skicka information om bröllopet"
+        )
+    )
+    verified = models.BooleanField(_("Kvittens skickad"), default=False)
+
+    class Meta:
+        verbose_name = _("Svar på inbjudan")
+        verbose_name_plural = _("Svar på inbjudan")
+
+    def __str__(self):
+        return self.name
+
+
+class RVSPForm(ModelForm):
+    class Meta:
+        model = InvitationResponse
+        exclude = []
+        widgets = {
+            "is_coming": RadioSelect,
+            "is_partying": RadioSelect,
+            "camping": RadioSelect,
+        }
