22825bb973bd59b08990f170f8d9887f7a3e0f4a
Anders Ytterström <yttan@fastmail.se>
2023-04-30 12:32:45 +0200

    Blacken all files

--- src/invitation/migrations/0004_alter_invitationresponse_children_count_and_more.py
+++ src/invitation/migrations/0004_alter_invitationresponse_children_count_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("invitation", "0003_alter_invitationresponse_camping_and_more"),
     ]
